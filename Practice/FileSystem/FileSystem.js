96d60e15b93277d80279b6203b1e3237212991c7
Sonu Kumar <sonu.patna0808@gmail.com>
2021-05-23 19:21:54 +0530

    Create a new File inside a Folder Temp

--- Practice/FileSystem/FileSystem.js
+++ Practice/FileSystem/FileSystem.js
@@ -11,3 +11,13 @@ fs.mkdir("Temp", (err) => {
     console.log(err);
   }
 });
+
+// 2.  Create File inside previously created Folder
+
+const data =
+  "This is the test data which will push into new file recently created.";
+
+fs.writeFile("Temp/temp.txt", data, (err) => {
+  if (!err) console.log("Data Pushed");
+  else console.log(err);
+});
