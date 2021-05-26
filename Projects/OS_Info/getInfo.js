d420044f55cfc2a24eacf686eea594181a6d2d62
Sonu Kumar <sonu.patna0808@gmail.com>
2021-05-26 23:15:02 +0530

    New Project added

---
+++ Projects/OS_Info/getInfo.js
@@ -0,0 +1,7 @@
+const os = require("os");
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => res.send(os));
+app.listen(port, () => console.log(`Example app listening on port port!`));
